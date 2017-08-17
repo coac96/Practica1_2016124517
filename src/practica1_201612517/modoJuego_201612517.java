216b77433c2b72e71ffc424c590029a65b6a873e
Coac_96 <Coac_96@coac96-PC>
2017-08-16 21:29:23 -0600

    creacion metodo VOLTEAR

--- src/practica1_201612517/modoJuego_201612517.java
+++ src/practica1_201612517/modoJuego_201612517.java
@@ -4,34 +4,44 @@
  * and open the template in the editor.
  */
 package practica1_201612517;
+import java.util.Scanner;
+
+import javax.management.Query;
 
 /**
  *
  * @author Coac_96
  */
 public class modoJuego_201612517 {
-    private int[][] tablero;
+    private String[][] tablero;
+    private int mod = 0;
+    private Scanner elec = new Scanner(System.in);
 
     public modoJuego_201612517() {
     }
     
     public void principiante(){
-        tablero = new int[4][4];
+        mod = 4;
+        tablero = new String[4][4];
         for(int fila = 0; fila<4; fila++){
             for(int columna = 0; columna<4; columna++){
+                tablero[fila][columna] = "x";
                 System.out.print(" | "+ tablero[fila][columna] +" | ");
                 if(columna == 3){
                     System.out.println("");
                 }
             }
         }
-        
     }
     
     public void intermedio(){
-        tablero = new int[6][6];
+        tablero = new String[6][6];
     }
     public void experto(){
-        tablero = new int[8][8];
+        tablero = new String[8][8];
+    }
+    
+    public void voltear(){
+        
     }
 }
