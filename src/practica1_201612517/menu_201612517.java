3945e4eb274c188dff92893edf79bac60db39353
Coac_96 <Coac_96@coac96-PC>
2017-08-16 20:33:37 -0600

    Tarea 3 Finalizada con exito

---
+++ src/practica1_201612517/menu_201612517.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package practica1_201612517;
+import java.util.Scanner;
+/**
+ *
+ * @author Coac_96
+ */
+public class menu_201612517 {
+    Scanner lector = new Scanner(System.in);
+    public menu_201612517() {
+    }
+    public void menu(){
+        System.out.println("BUSCA MINAS");
+    }
+}
