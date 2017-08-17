00dff5e3d72cc5ac4296a8b8036086d89a76ea9b
Coac_96 <Coac_96@coac96-PC>
2017-08-16 21:16:27 -0600

    Creacion tablero 4X4

--- src/practica1_201612517/modoJuego_201612517.java
+++ src/practica1_201612517/modoJuego_201612517.java
@@ -10,18 +10,28 @@ package practica1_201612517;
  * @author Coac_96
  */
 public class modoJuego_201612517 {
+    private int[][] tablero;
 
     public modoJuego_201612517() {
     }
     
     public void principiante(){
+        tablero = new int[4][4];
+        for(int fila = 0; fila<4; fila++){
+            for(int columna = 0; columna<4; columna++){
+                System.out.print(" | "+ tablero[fila][columna] +" | ");
+                if(columna == 3){
+                    System.out.println("");
+                }
+            }
+        }
         
     }
     
     public void intermedio(){
-        
+        tablero = new int[6][6];
     }
     public void experto(){
-        
+        tablero = new int[8][8];
     }
 }
