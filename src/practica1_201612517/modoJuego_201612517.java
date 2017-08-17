74f53e29d1d86bc52d73d199c4fc64f341de2534
Coac_96 <Coac_96@coac96-PC>
2017-08-16 20:47:07 -0600

    Creacion de clases: menu y modoJuego, con sus respectivos metodos para hacer funcionar el juego

---
+++ src/practica1_201612517/modoJuego_201612517.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package practica1_201612517;
+
+/**
+ *
+ * @author Coac_96
+ */
+public class modoJuego_201612517 {
+
+    public modoJuego_201612517() {
+    }
+    
+    public void principiante(){
+        
+    }
+    
+    public void intermedio(){
+        
+    }
+    public void experto(){
+        
+    }
+}
